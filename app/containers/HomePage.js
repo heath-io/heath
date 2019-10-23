5765491bc85928013477c78c09550f8c851a67f3
wangzhenfu <wangzhenfu@zto.com>
2019-10-23 10:41:00 +0800

    initial

---
+++ app/containers/HomePage.js
@@ -0,0 +1,15 @@
+// @flow
+import React, { Component } from 'react';
+
+type Props = {};
+
+export default class HomePage extends Component<Props> {
+
+  render() {
+    return (
+      <div>
+        <p>Hello React</p>
+      </div>
+    )
+  }
+}
